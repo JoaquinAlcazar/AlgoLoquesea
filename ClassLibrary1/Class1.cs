3dd818eb9c91dc1edfe6fab0bb55534ef3fea88e
joaquinalcazar <joaquin.alcazar.7e4@itb.cat>
2024-03-06 12:21:37 +0100

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace library
+{
+    public class employee
+    {
+        protected DateOnly birthDate { get; set; }
+
+        public employee(DateOnly _birthdate) {
+            this.birthDate = _birthdate;
+        }
+        public int countage() 
+        {
+            return 2024 - this.birthDate.Year;
+        }
+        
+    }
+}
---
+++ ConsoleAppUF4/Program.cs
@@ -0,0 +1,14 @@
+﻿using library;
+
+namespace UF4
+{
+    public class Exercici1
+    {
+        public static void Main(String[] args) 
+        {
+            employee empleao = new employee (new DateOnly(2000, 10, 18));
+            Console.WriteLine(empleao.countage());
+    
+        }
+    }
+}
